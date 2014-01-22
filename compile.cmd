d68a7e3adb52304d67a01e3c59cda5217514cb13
Balazs Varga <bb.varga@gmail.com>
2014-01-22 22:45:17 +0100

    -assets init moved to assets.js -using jade templates

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,2 @@
 call .\node_modules\.bin\stylus stylus --compress --inline --out public/css
-rem call .\node_modules\.bin\jade views\templates --out public\templates
-rem call .\node_modules\.bin\jade views\templates\graph --out public\templates\graph
+call .\node_modules\.bin\jade views --out public\templates
--- server.js
+++ server.js
@@ -1,79 +1,35 @@
 var config = require('./config')
   , express = require('express')
-  , doT = require('dot')
   , app = express()
   , http = require('http')
-  , fs = require('fs')
-  , assets = {}
+  , assets = require('./lib/assets')(config.release)
   ;
 
-// read the template file
-fs.readFile('./views/main.dot', { encoding: 'utf8' }, function(err,data){
-  assets.template = doT.template(data);
-});
-
-
-
-if (config.release) {
-  fs.readdir('./release/js', function(err,files){
-    if (err)
-      return console.error('error reading assets', err);
-    for(var i = 0; i < files.length; i++){
-      if ( files[i].match(/^login-/))
-        assets.loginjs = '/js/' + files[i]
-      else if ( files[i].match(/^home-/))
-        assets.homejs = '/js/' + files[i];
-    }
-  });
-  
-  fs.readdir('./release/css', function(err,files){
-    if (err)
-      return console.error('error reading assets', err);
-    for(var i = 0; i < files.length; i++){
-      if ( files[i].match(/^login-/))
-        assets.logincss = '/css/' + files[i]
-      else if ( files[i].match(/^home-/))
-        assets.homecss = '/css/' + files[i]
-      else if ( files[i] == 'logininline.css' )
-        assets.logininlinestyle = fs.readFileSync( './release/css/' + files[i], { encoding: 'utf8'})
-      else if ( files[i] == 'homeinline.css' )
-        assets.homeinlinestyle = fs.readFileSync( './release/css/' + files[i], { encoding: 'utf8'});
-    }
-  });
-  
-
-} else {
-  assets.requirejs = '/js/lib/require.js';
-  assets.requirejsconfig = '/js/config.js';
-  assets.loginjs = '/js/login.js';
-  assets.homejs = '/js/home.js';
-  assets.logincss = '/css/login.css';
-  assets.homecss = '/css/home.css';
-  assets.logininlinestyle = '/css/logininline.css';
-  assets.homeinlinestyle = '/css/homeinline.css';
-}
-
+console.log('assets', assets);
 app.configure(function(){
   app.set('port', config.port);
   app.use(express.static(config.staticPath));
 });
 
 app.get( '*',  function( req, res, next ){
-  console.log('get', assets);
+  // params for the inlined body template
+  var inlinebodyparams = {
+    caption: 'hey'
+  };
+  var bootstraps = { 
+    user: req.user ? req.user.toJSON() : null,
+    license: true 
+  };
   res.end(assets.template({
-    release: config.release,
-    requirejs: !config.release ? assets.requirejs : null,
-    requirejsconfig: !config.release ? assets.requirejsconfig : null,
+    requirejs: assets.requirejs,
+    requirejsconfig: assets.requirejsconfig,
     assetjs: req.user ? assets.homejs : assets.loginjs,
     assetcss: req.user ? assets.homecss : assets.logincss,
     inlinestyle: req.user ? assets.homeinlinestyle : assets.logininlinestyle,
-    
-    user: req.user ? req.user.toJSON() : null,
-    caption: 'hey',
-    bs: JSON.stringify({ user: 'bob', license: true }) 
+    body: req.user ? assets.homebody(inlinebodyparams) : assets.loginbody(inlinebodyparams),
+    bs: JSON.stringify(bootstraps) 
   }));
 });
-
 
 server = http.createServer(app).listen(config.port);
 console.log( 'Server is listening on port', config.port );
